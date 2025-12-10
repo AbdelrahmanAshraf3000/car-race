2c8b38d9d2445bf9e7e81193f128666085614874
joex003 <youssefaymann22@gmail.com>
2025-12-10 17:05:50 +0200

    rigidbody

---
+++ source/common/components/rigidbody.cpp
@@ -0,0 +1,25 @@
+#include "rigidbody.hpp"
+#include "../ecs/entity.hpp"
+#include "../deserialize-utils.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+namespace our
+{
+
+    void RigidbodyComponent::deserialize(const nlohmann::json& data)
+    {
+        if (!data.is_object())
+            return;
+
+        position = data.value("position", position);
+        rotation = glm::radians(data.value("rotation", rotation)); // Convert degrees to radians
+        mesh = data.value("mesh", mesh);
+        mass = data.value("mass", mass);
+        scale = data.value("scale", scale);
+        input = data.value("input", input);
+        steeringAngle = data.value("steeringAngle", steeringAngle);
+    }
+}
